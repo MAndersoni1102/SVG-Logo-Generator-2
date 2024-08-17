0399f4c3df93ff0ac2195510050a101e600fb425
Michael Anderson <mandersonisd@gmail.com>
2024-08-16 22:42:03 -0600

    Tweaked index.js by adding validations to ensure the user puts in the required text such as a valid shape or valid color

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
-const inquirer = require('inquirer');
-const fs = require('fs');
-const Logo = require('./lib/logo');
-
- inquirer.prompt([
-    {
-        type: 'input',
-        name: 'text',
-        message: 'Enter Logo Text',
-    },
-    {
-        type: 'input',
-        name: 'textColor',
-        message: 'Enter color for logo text',
-    },
-    {
-        type: 'list',
-        name: 'shape',
-        message: 'Pick a logo shape',
-        choices: ['Circle','Square', 'Triangle'],
-    },
-    {
-        type: 'input',
-        name: 'shapeColor',
-        message: 'What color would you like the shape to be?',
-    }
-]).then(answers => {
-    console.log(answers);
-    const logo = new Logo(answers.text, answers.textColor, answers.shape, answers.shapeColor);
-
-    const logoSVG = logo.render();
-
-    console.log(logoSVG)
-
-    fs.writeFile('./examples/logo.svg', logoSVG, err => {
-        if (err) throw err;
-        console.log('file created');
+import("inquirer").then((inquirer) => {
+    const fs = require("fs");
+  const shapes = require("./lib/shapes");
+  
+    const questions = [
+      {
+        type: "input",
+        name: "text",
+        message: "Enter Logo Text",
+        validate: function (value) {
+          if (value.length <= 3) {
+            return true;
+          }
+          return "Please enter up to three characters.";
+        },
+      },
+      {
+        type: "input",
+        name: "textColor",
+        message: "Enter color for logo text:",
+        validate: function (value) {
+          const isValidColor =
+            /^#([0-9A-F]{3}){1,2}$/i.test(value) || /^([a-z]+)$/i.test(value);
+          if (isValidColor) {
+            return true;
+          }
+          return "Please enter a valid color.";
+        },
+      },
+      {
+        type: "list",
+        name: "shape",
+        message: "Pick a logo shape",
+        choices: ["Circle", "Triangle", "Square"],
+      },
+      {
+        type: "input",
+        name: "shapeColor",
+        message: "What color would you like your shape to be?",
+        validate: function (value) {
+          const isValidColor =
+            /^#([0-9A-F]{3}){1,2}$/i.test(value) || /^([a-z]+)$/i.test(value);
+          if (isValidColor) {
+            return true;
+          }
+          return "Please enter a valid color.";
+        },
+      },
+    ];
+  
+    inquirer.default.prompt(questions).then((answers) => {
+      const shape = new shapes[answers.shape](answers.shapeColor);
+      const svg = `<svg height="200" width="300" xmlns="http://www.w3.org/2000/svg">
+      ${shape.render()}
+      <text x='90' y='125' font-family='cursive' font-size='60' fill="${answers.textColor}">${answers.text}</text>
+    </svg>`
+     
+  
+      fs.writeFileSync("logo.svg", svg);
+      console.log("Generated logo.svg");
     });
-});
+  });
